890775597829e23972057781354d978a41972531
Yer <Yer@DESKTOP-NAV8814>
2021-09-30 13:16:07 +0400

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ll = new List<int>(4);
+            _List ll1 = new _List();
+            ll1.Add(12);
+            ll1.Add(13);
+            ll1.Add(14);
+            ll1.Add(15);
+            ll1.Add(16);
+        }
+        
+        
+    }
+}
---
+++ List/_List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class _List
+    {
+        private int[] _items;
+        int size;
+        private int _capacity = 4;
+        public _List()
+        {
+            _items = new int[_capacity];
+        }
+        public _List(int capacity)
+        {
+            _items = new int[capacity];
+            _capacity = capacity;
+
+        }
+        public void Add(int source)
+        {
+            if (_items.Length > size)
+            {
+                _items[size] = source;
+                size++;
+            }
+            else
+            {
+                _capacity = _capacity * 2;
+                Array.Resize(ref _items, _capacity);
+                _items[size] = source;
+                size++;
+            }
+
+
+        }
+
+    }
+}
